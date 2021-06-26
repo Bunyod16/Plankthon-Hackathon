3dfc371ef26d9d6406761e4c595e0450146e246d
Bunyod16 <71476894+Bunyod16@users.noreply.github.com>
2021-06-27 00:13:48 +0800

    added files

---
+++ build.sh
@@ -0,0 +1,23 @@
+##############################################################################
+# File: build.sh
+# Project: hackathon-plankton
+# Created Date: Friday, June 18th 2021, 10:33:29 pm
+# Author: ZHANG Xiyu
+# -----
+# Last Modified: Saturday, 19th June 2021 4:31:27 pm
+# Modified By: ZHANG Xiyu
+# -----
+# Copyright (c) 2021 Veolia S.A.
+# Please add license text here.
+# Nope...we're doomed!
+# -----
+# HISTORY:
+# Date      	By	Comments
+# ----------	---	----------------------------------------------------------
+##############################################################################
+### START CODE HERE (replace codes below with your codes) ###
+# Replace `zacharietimothee/hackathon-plankton-server`
+# by `your username/your repository:your tag` on Docker Hub.
+#sudo docker build -t bunyod16/plankathon:latest
+docker build -t bunyod16/plankathon:latest
+### END CODE HERE ###
